c39b6976142371f8087f0d4df60c17107d4dc2a3
pavithra22 <pavi2705@hotmail.fr>
2015-04-24 15:47:15 +0200

    First commit

--- src/WebSite/Controller/UserController.php
+++ src/WebSite/Controller/UserController.php
@@ -24,8 +24,23 @@ class UserController {
      */
     public function listUserAction($request) {
         //Use Doctrine DBAL here
-        $request=$this->get('database_connection');
-        $users = $request->fetchAll('SELECT * FROM users');
+        $config = new \Doctrine\DBAL\Configuration();
+        //for this array use config_dev.yml and YamlComponents
+        // http://symfony.com/fr/doc/current/components/yaml/introduction.html
+        $connectionParams = array(
+            'dbname' => 'tweeter',
+            'user' => 'users',
+            'password' => '',
+            'host' => 'localhost',
+            'driver' => 'pdo_mysql',
+        );
+        
+        $conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
+        // http://docs.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/data-retrieval-and-manipulation.html
+        // it's much better if you use QueryBuilder : http://docs.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/query-builder.html
+        $statement = $conn->prepare('SELECT * FROM user');
+        $statement->execute();
+        $users = $statement->fetchAll();
 
         //you can return a Response object
         return [
